b06c4ba3ef05b51397d906fedecb1c78944fd4b8
AinSophAur <ahh8096@gmail.com>
2026-05-01 05:49:30 +0900

    Add unique index to User.Email for database enforcement
    Unique constraint to make sure not to create duplicated users with email

--- services/Api/Domain/Users/Domain/User.cs
+++ services/Api/Domain/Users/Domain/User.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Api.Domain.Users.Domain
 {
+    [Index(nameof(Email), IsUnique = true)]
     public class User
     {
         [Key]
