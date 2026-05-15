75a0be5c8050c4e4f60829132d148b30f9ff600d
AinSophAur <ahh8096@gmail.com>
2026-05-15 13:40:26 +0900

    chores: DTO documentation for Swagger

--- services/Api/Domain/Comments/Dto/CommentRequestDto.cs
+++ services/Api/Domain/Comments/Dto/CommentRequestDto.cs
@@ -1,12 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Api.Domain.Comments.Dto
 {
     public record CommentCreateRequestDto
     {
         [Required]
+        [SwaggerSchema(Description = "Comment content")]
+        [DefaultValue("Thanks for sharing your post.")]
         public string Content { get; init; }
+
         [Required]
+        [SwaggerSchema(Description = "Post Id")]
+        [DefaultValue(1)]
         public long PostId { get; init; }
     }
 }
