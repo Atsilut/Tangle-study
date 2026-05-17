dbc8267f3197d1864c84fa6a32f9bc3a28db25f3
AinSophAur <ahh8096@gmail.com>
2026-05-17 18:35:15 +0900

    fix: Allow null return of GetCommentByIdAsync to solve CS8602

--- services/Api/Domain/Comments/Repository/ICommentRepository.cs
+++ services/Api/Domain/Comments/Repository/ICommentRepository.cs
@@ -5,7 +5,7 @@ namespace Api.Domain.Comments.Repository
     public interface ICommentRepository
     {
         public Task CreateCommentAsync(Comment comment);
-        public Task<Comment>? GetCommentByIdAsync(long id);
+        public Task<Comment?> GetCommentByIdAsync(long id);
         public Task<List<Comment>?> GetCommentsByPostIdAsync(long postId);
         public Task<List<Comment>?> GetCommentsByUserIdAsync(long userId);
         public Task UpdateCommentAsync(Comment comment);
