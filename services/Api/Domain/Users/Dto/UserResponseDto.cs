eee7c3ad488508f05e95ea79effd0fa4fafe460f
AinSophAur <ahh8096@gmail.com>
2026-05-20 10:27:02 +0900

    refactor: Removed unused reference

--- services/Api/Domain/Users/Dto/UserResponseDto.cs
+++ services/Api/Domain/Users/Dto/UserResponseDto.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Api.Domain.Users.Dto
+﻿namespace Api.Domain.Users.Dto
 {
     public record UserGetResponseDto(long Id, string Email, string Nickname, DateTime CreatedAt, DateTime UpdatedAt);
     public record UserPatchResponseDto(string Nickname, DateTime UpdatedAt);
